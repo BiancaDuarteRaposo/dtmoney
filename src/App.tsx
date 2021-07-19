8fb33a87130466941fd16b13e77bffa5fc92b96d
Bianca Duarte <bianca.du.raposo@gmail.com>
2021-07-19 20:32:13 -0300

    Criação e estilização do Header e Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import styled from 'styled-components'
+
+import React from 'react';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header.tsx';
+import { GlobalStyle } from './styles/global'
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
 
