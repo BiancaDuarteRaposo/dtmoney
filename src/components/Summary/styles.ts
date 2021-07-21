d0031a0ab5293849f97214a15b2546d6f7c8d255
Bianca Duarte <bianca.du.raposo@gmail.com>
2021-07-21 16:21:57 -0300

    Criação e estilização do Summary e TransactionTable

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -11,11 +11,26 @@ export const Container = styled.div`
      padding:1.5rem 2rem;
      border-radius: 0.25rem;
      color: var(--text-title);
+
     header {
         display: flex;
         aling-items: center;
         justify-content: space-between;
      }
+
+     strong { 
+         display: block;
+         margin-top: 1rem;
+         font-size: 2rem;
+         font-weight: 500;
+         line-height: 3rem;
+
+     }
+
+     &.highlight-background{
+         background: var(--green);
+         color: #fff;
+     }
     }
- 
+
 `;
